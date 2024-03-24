7c2b5b1285b4b0fe9f17b385f6023abb16c75a34
ji0ne <zi0neeeeeeeee@gmail.com>
2024-03-24 21:44:36 +0900

    Settings

---
+++ app/src/main/java/net/hananet/bns2/poppiyong/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.hananet.bns2.poppiyong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
