c6f473512aecfa3ce8872bd5f0f4e989f73ee937
ji0ne <zi0neeeeeeeee@gmail.com>
2024-04-07 16:14:36 +0900

    add xml + gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,26 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//추가
+    id ("kotlin-android")
+    id ("kotlin-kapt")
 }
+
 
 android {
     namespace = "net.hananet.bns2.poppiyong"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures{
+        dataBinding = true
+        viewBinding = true
+    }
+
 
     defaultConfig {
         applicationId = "net.hananet.bns2.poppiyong"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +43,26 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+
 }
-
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //추가
+    implementation("androidx.fragment:fragment-ktx:1.3.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+
+}
+kapt{
+    generateStubs = true
 }
